a084760b5a681b7d5955f5302797a7334e0fbd22
AnaMilenaE <aespinosaj@unal.edu.co>
2021-11-21 03:55:35 -0500

    avances en la lista de lugares

---
+++ app/src/main/java/com/karendamore/metabile/Lugar.kt
@@ -0,0 +1,4 @@
+package com.karendamore.metabile
+
+class Lugar {
+}
---
+++ app/src/main/java/com/karendamore/metabile/SitiosTuristicosListActivity.kt
@@ -0,0 +1,11 @@
+package com.karendamore.metabile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SitiosTuristicosListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sitios_turisticos_list)
+    }
+}
